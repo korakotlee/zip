a216930d7864ba6a90584250a8884158d8f4bc46
Korakot Leemakdej <kleemakdej@gmail.com>
2021-10-05 08:50:30 -0700

    init

---
+++ spec/requests/zip_code_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "ZipCodes", type: :request do
+
+  describe "ZipCode" do
+    # pending "add some examples (or delete) #{__FILE__}"
+    it "get zip code form api" do
+      
+  end
+end
