b898ea17b884ad1eb4db9fbb3c0babb01a94f16c
Korakot Leemakdej <kleemakdej@gmail.com>
2021-10-07 06:21:47 -0700

    sort1

--- app/models/zip_code_endpoint.rb
+++ app/models/zip_code_endpoint.rb
@@ -1,17 +1,35 @@
-ZIP_CODE_API_KEY = 'OPZ5C46YqxIfKbeJK4hDQwkzYn7T0743vSrXn4cBZR4vuo9lX9JEiwSVwwr37YGN'
+ZIP_CODE_API_KEY = "OPZ5C46YqxIfKbeJK4hDQwkzYn7T0743vSrXn4cBZR4vuo9lX9JEiwSVwwr37YGN"
+require 'benchmark'
 
 class ZipCodeEndpoint
   class << self
     def get_zip_in_radius(zipcode, radius, units = "mile")
       # 1. Get list of ZIP codes in radius from starting zip code point
-        extracted_zips_from_api = extract_zip_codes_from_api(zipcode, radius, units)
-      return []
-      #   return [] unless extracted_zips_from_api
+      zips_api = extract_zip_codes_from_api(zipcode, radius, units)
+      return [] unless zips_api
+      # 2. find all ZIP codes in the DB that match the response, and sort by tier ASC
+      zips_db = extract_zip_codes_from_db(zips_api["zip_codes"]).to_a
+      # 3. Sort ZIP codes in the same tier by distance
+      puts "ZipCodeEndpoint"
+      bm = Benchmark.realtime { 
+        sort_extracted_zips_from_db_by_distance(zips_api["zip_codes_with_distance"], zips_db) 
+      }
+      puts "*** benchmark 1: #{bm}"
+
+      zips_db = extract_zip_codes_from_db(zips_api["zip_codes"]).to_a
+      bm = Benchmark.realtime { 
+        sort1(zips_api["zip_codes_with_distance"], zips_db) 
+      }
+      puts "*** benchmark 2: #{bm}"
+
+
+      format_response(zips_db, zips_api['zip_codes_with_distance'])
     end
+
+    private
 
     def extract_zip_codes_from_api(zipcode, radius, units)
       zip_data = call_zip_codes_api(zipcode, radius, units)
-      byebug
       return nil unless zip_data
 
       tmp_array_of_zips = { "zip_codes" => [], "zip_codes_with_distance" => {} }
@@ -22,12 +40,70 @@ class ZipCodeEndpoint
       tmp_array_of_zips
     end
 
+    def extract_zip_codes_from_db(zipcodes)
+      tier_values = ["A", "B", "C"]
+      PartnerClinic.where("zipcode IN (?)", zipcodes).where("tier IN (?)", tier_values).order("tier ASC")
+    end
+
+    def sort_extracted_zips_from_db_by_distance(zip_codes_with_distance, zip_codes_from_db)
+      i = 0
+      tier_marker_begin = 0
+      while i < zip_codes_from_db.length
+        if (i == zip_codes_from_db.length - 1) || (zip_codes_from_db[i + 1].tier != zip_codes_from_db[i].tier)
+          (tier_marker_begin..i - 1).each do |j|
+            (tier_marker_begin..(i - 1 - j + tier_marker_begin)).each do |k|
+              if compare_zip_distances(zip_codes_from_db[k].zipcode, zip_codes_from_db[k + 1].zipcode, zip_codes_with_distance)
+                swap_zip_codes(k, k + 1, zip_codes_from_db)
+              end
+            end
+          end
+          tier_marker_begin = i + 1
+        end
+        i += 1
+      end
+    end
+
+    def sort1(zip_codes_with_distance, zips_db)
+      zips_db.sort! {|a ,b| 
+        [a.tier, zip_codes_with_distance[a.zipcode]] <=> [b.tier, zip_codes_with_distance[b.zipcode]]
+      }
+    end
+
+    def format_response(sorted_zips, zip_codes_with_distance)
+      response = []
+      sorted_zips.each do |item|
+        tmp_item = {
+          name: item.name,
+          address: item.address,
+          city: item.city,
+          state: item.state,
+          distance: zip_codes_with_distance[item.zipcode],
+          tier: item.tier,
+          contact_email: item.contact_email,
+          contact_name: item.contact_name
+        }
+        response.push(tmp_item)
+      end
+      response
+    end
+
     def call_zip_codes_api(zipcode, radius, units)
       url = "https://www.zipcodeapi.com/rest/#{ZIP_CODE_API_KEY}/radius.json/#{zipcode}/#{radius}/#{units}"
       zip_response = Faraday.send(:post, url)
       return nil unless zip_response.success?
 
       JSON.parse(zip_response.body)
+    end
+
+    def compare_zip_distances(zip1, zip2, zip_codes_with_distance)
+      # use zip_codes as keys to get the distances from zip_codes_with_distance
+      zip_codes_with_distance[zip1] > zip_codes_with_distance[zip2]
+    end
+
+    def swap_zip_codes(index_src, index_dst, zip_codes)
+      tmp = zip_codes[index_src]
+      zip_codes[index_src] = zip_codes[index_dst]
+      zip_codes[index_dst] = tmp
     end
   end
 end
--- spec/models/zip_code_endpoint_spec.rb
+++ spec/models/zip_code_endpoint_spec.rb
@@ -1,10 +1,30 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe ZipCodeEndpoint, type: :model do
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
+
+describe ZipCodeEndpoint, type: :model do
+  fixtures :partner_clinic
+  before do
+    @zipcode = "85022"
+    @radius = 5
+  end
   it "get zip code form api" do
-    zipcode = '85022'
-    radius = 5
-    zip = ZipCodeEndpoint.get_zip_in_radius(zipcode, radius)
-    expect(zip.length).to be > 0
+    zips_api = []
+    VCR.use_cassette("zip") do
+      zips_api = ZipCodeEndpoint.send(:extract_zip_codes_from_api, @zipcode, @radius, "mile")
+      expect(zips_api.length).to be > 0
+    end
+    partner_clinics = ZipCodeEndpoint.send(:extract_zip_codes_from_db, zips_api["zip_codes"]).to_a
+    expect(partner_clinics.length).to eq(3)
+  end
+
+  it "get zip codes and sort" do
+    VCR.use_cassette("zip") do
+      @out = ZipCodeEndpoint.get_zip_in_radius(@zipcode, @radius, "mile")
+    end
+    expect(@out.map {|r| r[:name]}).to eq(["Clinic 1", "Clinic 3", "Clinic 2"])
   end
 end
